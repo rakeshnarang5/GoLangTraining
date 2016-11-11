9ed93e0b2cfab3e7bfefd7447e5342c731e17586
rakeshnarang5 <rakeshnarang5@gmail.com>
2016-11-11 12:19:17 +0530

    assignment 4: concurrent crawler problem

--- crawler_problem.go
+++ crawler_problem.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -16,71 +17,54 @@ type Lang struct {
 	Time  string
 }
 
-// func pfunc(lang Lang) lang Lang {
-
-// }
-
 func setNameURLLang(Name string, URL string) Lang {
-
 	temp := Lang{Name: Name, URL: URL}
-
 	return temp
-
 }
 
-func crawl(pfunc func(Lang), lang Lang) {
-
+func crawl(pfunc func(Lang), lang Lang, wg *sync.WaitGroup) {
+	defer wg.Done()
 	initial := time.Now()
-
 	resp, err := http.Get(lang.URL)
-
 	if err != nil {
 		return
 	}
 	defer resp.Body.Close()
-
 	body, _ := io.Copy(ioutil.Discard, resp.Body)
-
 	lang.Bytes = uint(body)
-
 	final := time.Since(initial).String()
-
 	lang.Time = final
-
 	pfunc(lang)
-
 }
 
 func pfunc(lang Lang) {
 	fmt.Println("\nGolang's default formatting")
 	fmt.Printf("%v\n", lang)
-
 	fmt.Println("JSON formatting")
-
 	b, err := json.Marshal(lang)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 	fmt.Println(string(b))
-
 }
 
 func main() {
-
 	site1 := setNameURLLang("Python", "http://python.org/")
 	site2 := setNameURLLang("Ruby", "http://ruby-lang.org/en/")
 	site3 := setNameURLLang("Golang", "http://golang.org/")
-
+	var wg sync.WaitGroup
+	var langs = []Lang{
+		site1,
+		site2,
+		site3,
+	}
 	initial := time.Now()
-
-	crawl(pfunc, site1)
-	crawl(pfunc, site2)
-	crawl(pfunc, site3)
-
+	for _, lang := range langs {
+		wg.Add(1)
+		go crawl(pfunc, lang, &wg)
+	}
+	wg.Wait()
 	final := time.Since(initial)
-
 	fmt.Println("Total time: \n", final)
-
 }
